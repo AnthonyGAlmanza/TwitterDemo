c6d4a0e037f57a93c3d8459089d8ac47d19739c0
Anthony <antman_2012@yahoo.com>
2021-03-03 15:40:13 -0600

    updated TweetService.java

--- twitterdemo/src/main/java/com/example/twitterdemo/service/TweetService.java
+++ twitterdemo/src/main/java/com/example/twitterdemo/service/TweetService.java
@@ -1,4 +1,36 @@
 package com.example.twitterdemo.service;
 
+import com.example.twitterdemo.model.Tweet;
+import com.example.twitterdemo.model.User;
+import com.example.twitterdemo.repository.TweetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TweetService {
+
+    @Autowired
+    private TweetRepository tweetRepository;
+
+    public List<Tweet> findAll() {
+        List<Tweet> tweets = tweetRepository.findAllByOrderByCreatedAtDesc();
+        return tweets;
+    }
+
+    public List<Tweet> findAllByUser(User user) {
+        List<Tweet> tweets = tweetRepository.findAllByUserOrderByCreatedAtDesc(user);
+        return tweets;
+    }
+
+    public List<Tweet> findAllByUsers(List<User> users){
+        List<Tweet> tweets = tweetRepository.findAllByUserInOrderByCreatedAtDesc(users);
+        return tweets;
+    }
+
+    public void save(Tweet tweet) {
+        tweetRepository.save(tweet);
+    }
+
 }
