cbe03c6692288f85c2f8bc1c2bd265c13af1400f
Anthony <antman_2012@yahoo.com>
2021-03-03 15:36:59 -0600

    updated TweetRepository.java

--- twitterdemo/src/main/java/com/example/twitterdemo/repository/TweetRepository.java
+++ twitterdemo/src/main/java/com/example/twitterdemo/repository/TweetRepository.java
@@ -1,4 +1,17 @@
 package com.example.twitterdemo.repository;
 
-public interface TweetRepository {
+import com.example.twitterdemo.model.Tweet;
+import com.example.twitterdemo.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TweetRepository extends CrudRepository<Tweet, Long> {
+
+    List<Tweet> findAllByOrderByCreatedAtDesc();
+    List<Tweet> findAllByUserOrderByCreatedAtDesc(User user);
+    List<Tweet> findAllByUserInOrderByCreatedAtDesc(List<User> users);
+
 }
