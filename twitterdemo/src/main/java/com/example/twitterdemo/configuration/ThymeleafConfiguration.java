370e3781ba1b2942398a58fc383ec81b7cbd280a
Anthony <antman_2012@yahoo.com>
2021-03-03 14:39:40 -0600

    updated ThymeleafConfiguration.java

--- twitterdemo/src/main/java/com/example/twitterdemo/configuration/ThymeleafConfiguration.java
+++ twitterdemo/src/main/java/com/example/twitterdemo/configuration/ThymeleafConfiguration.java
@@ -1,4 +1,14 @@
 package com.example.twitterdemo.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
+
+@Configuration
 public class ThymeleafConfiguration {
+
+    @Bean
+    public SpringSecurityDialect springSecurityDialect() {
+        return new SpringSecurityDialect();
+    }
 }
