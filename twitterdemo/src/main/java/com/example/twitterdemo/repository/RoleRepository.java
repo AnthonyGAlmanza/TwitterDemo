0528268baf637827c215359573b9eb3620538dde
Anthony <antman_2012@yahoo.com>
2021-03-04 11:48:58 -0600

    added annotation to RoleRepository.java

--- twitterdemo/src/main/java/com/example/twitterdemo/repository/RoleRepository.java
+++ twitterdemo/src/main/java/com/example/twitterdemo/repository/RoleRepository.java
@@ -2,7 +2,9 @@ package com.example.twitterdemo.repository;
 
 import com.example.twitterdemo.model.Role;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends CrudRepository<Role, Long> {
 
     Role findByRole(String role);
